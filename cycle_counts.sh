bbb081cfd7c5fcce26fdb7b28126d3ce9878fc60
Kevin Angstadt <angstadt@virginia.edu>
2018-03-31 15:37:07 -0400

    improved cycle_counts.sh

--- cycle_counts.sh
+++ cycle_counts.sh
@@ -3,4 +3,6 @@
 # $1 is the mnrl file
 # $2 is the input file
 
-./print_cycles $1 $2 | grep "^cycle:" | cut -d ':' -f 2 | uniq -c | awk '{ print $2 ", " $1}' | tail -n +2
+BASEDIR=$(dirname "$0")
+
+$BASEDIR/print_cycles $1 $2 | grep "^cycle:" | cut -d ':' -f 2 | uniq -c | awk '{ print $2 ", " $1}' | tail -n +2
